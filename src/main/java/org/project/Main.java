32f3d8b276885e8109725c5bce25637367e15416
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-06-21 16:58:07 -0300

    Possivel inicio

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -2,6 +2,8 @@ package org.project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo a biblioteca Saber Infinito");
+
+
     }
 }
