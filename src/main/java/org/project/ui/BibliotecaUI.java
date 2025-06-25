f9e81ebb55c733ab9595bf30ba71b6148f5b17ea
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-06-25 08:26:51 -0300

    Delete src/main/java/org/project/ui directory

--- src/main/java/org/project/ui/BibliotecaUI.java
+++
@@ -1,9 +0,0 @@
-package org.project.ui;
-
-public interface BibliotecaUI {
-    int escolhaAcao();
-    void criarMembro();
-    void buscaLivro();
-    void fazerEmprestimo();
-    void devolverLivro();
-}
