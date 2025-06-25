b25220aa2fe8226178b7e3d18f9ef8a19176ddfe
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2025-06-24 21:29:42 -0300

    voltei pq não sei voltar

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,12 +1,37 @@
 package org.project;
 
-import org.project.ui.TerminalBibliotecaUI;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int escolha;
+
         System.out.println("Bem vindo a biblioteca Saber Infinito");
-        var bibliotecaSaber = new TerminalBibliotecaUI();
-        bibliotecaSaber.escolhaAcao();
+
+        do{
+            System.out.println("O que deja fazer:" +
+                    "\n1) Cadastrar livros." +
+                    "\n2) Cadastrar membro." +
+                    "\n3) Fazer emprestimo." +
+                    "\n4) Devolver livro." +
+                    "\n0) Sair.");
+            escolha = scanner.nextInt();
+
+            switch (escolha){
+                case 1://cadastro de livro
+                    break;
+                case 2://cadastro de mebros
+                    break;
+                case 3://fazer emprestimo
+                    break;
+                case 4://devolver livro
+                    break;
+                default:
+                System.out.println("Saindo...");
+            }
+
+        }while (escolha!=0);
 
 
     }
